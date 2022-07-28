b3823d9909e3dabcb7dcdcd7d05ba54d224a3aff
johnthebeloved <johnoluwale1@gmail.com>
2022-07-28 14:53:02 -0400

    This is the contact class that allows Room to create a database for the app

---
+++ app/src/main/java/ca/johnoluwale/contactme/Contact.java
@@ -0,0 +1,38 @@
+package ca.johnoluwale.contactme;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import androidx.annotation.NonNull;
+
+@Entity(tableName = "contact")
+public class Contact {
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "Id")
+    private int cid;
+    @NonNull
+    @ColumnInfo(name = "Contact Name")
+    private String contactName;
+    @NonNull
+    @ColumnInfo(name = "Contact Email")
+    private String contactEmail;
+
+    //Constructor
+    public Contact(String name, String email){
+        this.contactName = name;
+        this.contactEmail = email;
+    }
+
+    //getter methods for the declared variables
+    public int getId() {
+        return cid;
+    }
+    public String getContactName(){
+        return contactName;
+    }
+    public String getContactEmail(){
+        return contactEmail;
+    }
+}
