343da1b988a5745ce289b6bac29c7fd872243671
johnthebeloved <johnoluwale1@gmail.com>
2022-08-03 11:50:56 -0400

    Update ContactRoomDatabase.java

--- app/src/main/java/ca/johnoluwale/contactme/ContactRoomDatabase.java
+++ app/src/main/java/ca/johnoluwale/contactme/ContactRoomDatabase.java
@@ -1,4 +1,35 @@
 package ca.johnoluwale.contactme;
 
-public class ContactRoomDatabase {
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+//Annotating the class to be a Room Database
+@Database(entities = {Contact.class}, version = 1, exportSchema = false)
+public abstract class ContactRoomDatabase extends RoomDatabase {
+
+    //Defining the Dao that will work with the database
+    public abstract ContactDao contactDao();
+
+    /*
+    Creating a singleton for the room database to avoid
+    multiple instances of the database opened at the same time
+     */
+    private static ContactRoomDatabase INSTANCE;
+    public static ContactRoomDatabase getDatabase(final Context context){
+        if (INSTANCE == null){
+            synchronized (ContactRoomDatabase.class){
+                if (INSTANCE == null){
+                    //Creates room database object using Room's database builder
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            ContactRoomDatabase.class, "contact_database")
+                             //Implementing a non-destructive Migration Strategy
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
